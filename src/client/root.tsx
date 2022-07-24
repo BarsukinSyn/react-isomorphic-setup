d2515c235fdd8a9b44149cd6d2a558f9a21741ca
Vladimir <v.asankin@gmail.com>
2022-07-24 14:30:00 +0400

    feat(styles): style loaders added

--- src/client/root.tsx
+++ src/client/root.tsx
@@ -6,11 +6,18 @@ export const documentSeparator = '-~-'
 
 export interface RootProps {
   jsFilePaths?: string[]
+  cssFilePaths?: string[]
 }
 
-export const Root: FC<RootProps> = ({ jsFilePaths = [] }) => {
+export const Root: FC<RootProps> = ({
+  jsFilePaths = [],
+  cssFilePaths = []
+}) => {
   const externalScriptTagList = jsFilePaths.map((src, i) => (
     <script defer src={src} key={i} />
+  ))
+  const styleSheetLinkTagList = cssFilePaths.map((href, i) => (
+    <link rel='stylesheet' href={href} key={i} />
   ))
 
   return (
@@ -21,6 +28,7 @@ export const Root: FC<RootProps> = ({ jsFilePaths = [] }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>React Isomorphic App</title>
         {externalScriptTagList}
+        {styleSheetLinkTagList}
       </head>
       <body>
         <noscript>You need to enable JavaScript to use this app</noscript>
