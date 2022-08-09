7d4b4320bb8dfb84df9d4467097f50e43a3606ea
Vladimir <v.asankin@gmail.com>
2022-08-09 18:03:19 +0400

    fix(streaming): move static views to layout component

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react'
+import React, { FC, Suspense, lazy } from 'react'
 import { Provider } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
 
-import { Catalog } from './catalog'
+import { Layout } from './shared/layout'
 import { Store } from './store'
+
+const Catalog = lazy(() => import('./catalog'))
 
 import './styles.scss'
 
@@ -14,7 +16,16 @@ export interface AppProps {
 export const App: FC<AppProps> = ({ store }) => (
   <Provider store={store}>
     <Routes>
-      <Route index element={<Catalog />} />
+      <Route path='/' element={<Layout />}>
+        <Route
+          index
+          element={
+            <Suspense fallback={<p>Flipping through an old book...</p>}>
+              <Catalog />
+            </Suspense>
+          }
+        />
+      </Route>
     </Routes>
   </Provider>
 )
--- src/client/catalog/index.ts
+++ src/client/catalog/index.ts
@@ -1,2 +1,2 @@
-export { CatalogContainer as Catalog } from './catalog.container'
+export { CatalogContainer as default } from './catalog.container'
 export { catalogSlice } from './catalog.slice'
